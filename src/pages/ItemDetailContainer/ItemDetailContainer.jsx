20b25283a6228db2f5655e9b3c9243ce1c92f035
torres30 <tomastorresdiaz01@gmail.com>
2022-05-09 11:08:01 -0300

    routing items

--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -6,18 +6,18 @@ import "./ItemDetailContainer.css"
 
 function getItem(id) {
     const myPromise = new Promise((resolve, reject) => {
-        const items = 
-            {id: 1, title: "Mesa de arrime", description: ".", price: 5000, pictureUrl: "../images/ma1.jpg"} /*
+        const items =[ 
+            {id: 1, title: "Mesa de arrime", description: ".", price: 5000, pictureUrl: "../images/ma1.jpg"},
             {id: 2, title: "Estanteria rombo", description: ".", price: 4000, pictureUrl: "../images/est1.jpg"},
             {id: 3, title: "Estanteria carrito", description: ".", price: 9000, pictureUrl: "../images/est2.jpg"},
             {id: 4, title: "Escritorio", description: ".", price: 17000, pictureUrl: "../images/esc2.jpg"},
             {id: 5, title: "Escritorio c/ estantes", description: ".", price: 20000, pictureUrl: "../images/esc1.jpg"},
-            {id: 6, title: "Mesa exterior", description: ".", price: 25000, pictureUrl: "../images/mg1.jpg"}] */;
+            {id: 6, title: "Mesa exterior", description: ".", price: 25000, pictureUrl: "../images/mg1.jpg"}];
 
-        /* const item = items.filter(item => item.id === parseInt(id)) */
+        const itemFil = items.filter(item => item.id === parseInt(id))
 
         setTimeout(() => {
-            resolve(items);
+            resolve(itemFil[0]);
         }, 0);
     });
     return myPromise
@@ -25,14 +25,14 @@ function getItem(id) {
 
 function ItemDetailContainer() {
 
-    const [item, setItems] = useState([]);
-    const {id} = useParams;
+    const [item, setItem] = useState([]);
+    const {id} = useParams();
 
 
     useEffect(() => {
         getItem(id)
             .then(res => {
-                setItems(res);
+                setItem(res);
             })
             .catch(err => {
                 console.log(err)
