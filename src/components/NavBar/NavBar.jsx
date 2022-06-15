2c43877d4b4bb711192b09e4f59b4d6c2f798bfd
torres30 <tomastorresdiaz01@gmail.com>
2022-06-15 19:42:54 -0300

    new images and links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,13 +13,13 @@ function NavBar(props) {
             </ul>
         </nav>
         <div className='logoWrapper'>
-            <Link to="/"><p className='navLogo'>mebel</p></Link>
+            <Link to="/"><p className='navLogo'>TRYHARDY</p></Link>
         </div>
         <nav className='nav__secondary'>
             <ul className='nav__list'>
-                <li className='nav__item'><NavLink to="/category/escritorios">Escritorios</NavLink></li>
-                <li className='nav__item'><NavLink to="/category/mesas">Mesas</NavLink></li>
-                <li className='nav__item'><NavLink to="/category/estanterias">Estanterías</NavLink></li>
+                <li className='nav__item'><NavLink to="/category/ram">Memorias RAM</NavLink></li>
+                <li className='nav__item'><NavLink to="/category/cpu">Procesadores</NavLink></li>
+                <li className='nav__item'><NavLink to="/category/gpu">Placas de video</NavLink></li>
                 <CartWidget />
             </ul>
         </nav>
